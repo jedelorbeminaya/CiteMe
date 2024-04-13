55b6a002790d45bc3af89e7a4555e537be494a61
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-04-12 21:28:30 -0400

    Init development 12/04/2024

---
+++ src/CiteMe.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CiteMe.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
