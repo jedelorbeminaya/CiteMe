c2eff2c26ae90104d815169fd40f08f8c6ffd420
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-04-16 17:26:43 -0400

    Update Cites.cs

--- src/CiteMe.Domain/Cites.cs
+++ src/CiteMe.Domain/Cites.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace CiteMe.Domain
 {
-    internal class Cites
+    public class Cites
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
     }
 }
