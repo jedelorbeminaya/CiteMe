e80c2ae1b1d76970807b1c5eb76b98b9b4f5c68c
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-04-16 17:25:52 -0400

    V1-first-step-16/04/2024

---
+++ src/CiteMe.Persistence/DataContex.cs
@@ -0,0 +1,20 @@
+﻿using CiteMe.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiteMe.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Cites> Cites { get; set; }
+    }
+}
--- src/CiteMe.Web/Program.cs
+++ src/CiteMe.Web/Program.cs
@@ -1,7 +1,14 @@
+using CiteMe.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("SqlMainConnection")));
+
 
 var app = builder.Build();
 
