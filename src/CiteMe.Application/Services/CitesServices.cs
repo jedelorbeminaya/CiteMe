81dfe27ffa7ed601e00846b29d04160a29a6bf0c
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-04-18 22:56:44 -0400

    Development-V1-18/04/2024

---
+++ src/CiteMe.Application/Services/CitesServices.cs
@@ -0,0 +1,109 @@
+﻿using CiteMe.Application.Contracts;
+using CiteMe.Domain;
+using CiteMe.Models;
+using CiteMe.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiteMe.Application.Services
+{
+    public class CitesServices : ICitesServices
+    {
+        private readonly DataContext _context;
+
+        public CitesServices(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CitesModel>> GetAll()
+        {
+
+            var citesDb = await _context.Cites.ToListAsync();
+            var cites = new List<CitesModel>();
+            foreach (var citeDb in citesDb)
+            {
+                cites.Add(new CitesModel
+                {
+
+                    id = citeDb.id,
+                    name = citeDb.name,
+                    telephone = citeDb.telephone,
+                    date = citeDb.date,
+                    time = citeDb.time
+
+                });
+            }
+            return cites;
+
+        }
+
+        public async Task<CitesModel> Get(int id)
+        {
+
+            //var citeDb = await _context.Cites.FindAsync(id);
+            var citeDb = await GetCite(id);
+            var cite = new CitesModel
+            {
+
+                id = citeDb.id,
+                name = citeDb.name,
+                telephone = citeDb.telephone,
+                date = citeDb.date,
+                time = citeDb.time
+
+            };
+
+            return cite;
+
+        }
+
+        public bool Create(CitesModel cites)
+        {
+            try
+            {
+                var citeDb = new Cites
+                {
+                    name = cites.name,
+                    telephone = cites.telephone,
+                    date = cites.date,
+                    time = cites.time
+
+                };
+                _context.Cites.Add(citeDb);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
+        public async Task<bool> Remove(CitesModel cites)
+        {
+            try
+            {
+                var citeDb = await GetCite(cites.id);
+                _context.Cites.Remove(citeDb);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
+
+        private async Task<Cites> GetCite(int id)
+        {
+
+            return await _context.Cites.FindAsync(id);
+
+        }
+
+    }
+}
--- src/CiteMe.Web/Controllers/CitesController.cs
+++ src/CiteMe.Web/Controllers/CitesController.cs
@@ -1,4 +1,7 @@
-﻿using CiteMe.Domain;
+﻿using CiteMe.Application.Contracts;
+using CiteMe.Application.Services;
+using CiteMe.Domain;
+using CiteMe.Models;
 using CiteMe.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,42 +10,54 @@ namespace CiteMe.Web.Controllers
 {
     public class CitesController : Controller
     {
-        private readonly DataContext _context;
-        public CitesController(DataContext context)
+        private readonly ICitesServices _citesServices;
+
+        public CitesController(ICitesServices citesServices)
         {
-            _context = context;
+            _citesServices = citesServices;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var citesDb = await _context.Cites.ToListAsync();
+            //var citesDb = await _context.Cites.ToListAsync();
+            var citesDb = await _citesServices.GetAll();
+
             return View(citesDb);
         }
 
         [HttpGet]
         public async Task<IActionResult> View(int id)
         {
-            var citesDb = _context.Cites.Find(id);
+            var citesDb = await _citesServices.Get(id);
             return View(citesDb);
         }
 
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            var citesDb = _context.Cites.Find(id);
+            var citesDb = await _citesServices.Get(id);
             return View(citesDb);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(Cites model)
+        public async Task<IActionResult> Delete(CitesModel model)
         {
-         
-            _context.Cites.Remove(model);
-            _context.SaveChanges();
+            //_context.Cites.Remove(model);
+            //_context.SaveChanges();
             //return View();
-            return RedirectToAction("Index");
+            //return RedirectToAction("Index");
             //return RedirectToAction(nameof(Index));
+            var state = await _citesServices.Remove(model);
+            if (state)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Error = "¡Ooops! Error Seving...";
+                return View(model);
+            }
         }
     }
 }
